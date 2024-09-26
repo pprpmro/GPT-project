e418caf9e4024d03224f044d56aa194f7498dcde
Roman Guskov <pprpmro@mail.ru>
2024-09-26 22:10:37 +0300

    Lab1 complete

--- SITBLabs/Lab1.cs
+++ SITBLabs/Lab1.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using System.Web;
-using GPTTest.Providers.ChatGPT;
 using HtmlAgilityPack;
 
 namespace SITBLabs
@@ -17,7 +11,37 @@ namespace SITBLabs
         public static Regex foundedYearRegex = new Regex(@"в\s+(\d{4})\s+году");
         public static Regex postIndexYearRegex = new Regex(@"Почтовый индекс\s+(\d{6})");
 
-        public static void Run()
+        public static string tulaNewsUrl = "https://tulacity.gosuslugi.ru/dlya-zhiteley/novosti-i-reportazhi/?cur_cc=40&curPos=";
+
+        public static object locker = new object();
+
+        public static async Task Run()
+        {
+            var commonList = new List<string>();
+
+            Parallel.For(0, 9, (i) =>
+            {
+                Console.WriteLine($"Beginning iteration {i}");
+
+                var localResult = GetTagsByIndex((i * 10).ToString());
+
+                lock(locker)
+                {
+                    commonList.AddRange(localResult);
+                }
+
+                Console.WriteLine($"Completed iteration {i}");
+            });
+
+            var result = commonList.GroupBy(x => x).Select(group => new { Tag = group.Key, Count = group.Count() }).OrderByDescending(x => x.Count).ToList();
+
+            foreach (var item in result)
+            {
+                await Console.Out.WriteLineAsync($"{item.Tag} : {item.Count}");
+            }
+        }
+
+        public static void KurkinoTestRun()
         {
             var url = "https://ru.wikipedia.org/wiki/%D0%9A%D1%83%D1%80%D0%BA%D0%B8%D0%BD%D0%BE_(%D0%A2%D1%83%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C)";
             var html = Task.Run(() => GetHtmlFromUrlAsync(url)).Result;
@@ -36,13 +60,48 @@ namespace SITBLabs
             var foundedYear = FindFoundedYear(pageText);
             Console.WriteLine($"Поселение основано в: {foundedYear}");
 
-            var foundedPostIndex = FindPostIndex(pageText);
-            Console.WriteLine($"Почтовый индекс: {foundedPostIndex}");
+            string ExtractTextFromBodyHtml(HtmlDocument htmlDocument)
+            {
+                var bodyNode = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"mw-content-text\"]");
+                if (bodyNode != null)
+                {
+                    return HttpUtility.HtmlDecode(bodyNode.InnerText);
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
 
-            var gptDialog = new ChatGPTDialog();
-            gptDialog.SetSystemPrompt(" ");
+            string FindPopulation(string text)
+            {
+                return FindDataByRegex(text, populationRegex);
+            }
+
+            string FindFoundedYear(string text)
+            {
+                return FindDataByRegex(text, foundedYearRegex);
+            }
         }
 
+        public static List<string> GetTagsByIndex(string startIndex = "0")
+        {
+            var url = tulaNewsUrl + startIndex;
+
+            var html = Task.Run(() => GetHtmlFromUrlAsync(url)).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            if (htmlDocument is null)
+            {
+                return new List<string>();
+            }
+
+            return htmlDocument.DocumentNode
+                .SelectNodes("//span[@class=\"item-category tpl-text-alt\"]")
+                .Select(x => x.InnerText.Trim())
+                .ToList();
+        }
 
         static async Task<string> GetHtmlFromUrlAsync(string url)
         {
@@ -61,34 +120,6 @@ namespace SITBLabs
                     return string.Empty;
                 }
             }
-        }
-
-        static string ExtractTextFromBodyHtml(HtmlDocument htmlDocument)
-        {
-            var bodyNode = htmlDocument.DocumentNode.SelectSingleNode("//body");
-            if (bodyNode != null)
-            {
-                return HttpUtility.HtmlDecode(bodyNode.InnerText);
-            }
-            else
-            {
-                return string.Empty;
-            }
-        }
-
-        static string FindPopulation(string text)
-        {
-            return FindDataByRegex(text, populationRegex);
-        }
-
-        static string FindFoundedYear(string text)
-        {
-            return FindDataByRegex(text, foundedYearRegex);
-        }
-
-        static string FindPostIndex(string text)
-        {
-            return FindDataByRegex(text, postIndexYearRegex);
         }
 
         static string FindDataByRegex(string text, Regex regex)
