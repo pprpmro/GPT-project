2b89b4ba3365c9782eb4c772abf209fb67344c62
Roman Guskov <pprpmro@mail.ru>
2025-05-09 01:50:29 +0300

    DeepSeek dialog

---
+++ GPTProject.Providers/Dialogs/Implementations/DeepSeekDialog.cs
@@ -0,0 +1,15 @@
+﻿using GPTProject.Providers.Data;
+using static GPTProject.Providers.Common.Configurations.DeepSeek;
+
+namespace GPTProject.Providers.Dialogs.Implementations
+{
+	public class DeepSeekDialog : BaseChatDialog<Message, Request>
+	{
+		public DeepSeekDialog(int maxDialogHistorySize = 50)
+			: base(DialogModels.Default, DialogCompletionsEndpoint)
+		{
+			httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {ApiKey}");
+			MaxDialogHistorySize = maxDialogHistorySize;
+		}
+	}
+}
