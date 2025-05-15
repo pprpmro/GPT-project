e971607800218f3f5fdd0f3f8f27764439838665
AngelinaVybornova <95091497+AngelinaVybornova@users.noreply.github.com>
2025-05-16 00:01:00 +0300

    Character fix

--- GPTProject.Core/Data/Character.cs
+++ GPTProject.Core/Data/Character.cs
@@ -1,7 +1,4 @@
-﻿using System.Text.Json.Serialization;
-using System.Text.Json;
-using GPTProject.Providers.Dialogs.Enumerations;
-using GPTProject.Common;
+﻿using GPTProject.Providers.Dialogs.Enumerations;
 
 namespace GPTProject.Core.Data
 {
@@ -15,16 +12,19 @@ namespace GPTProject.Core.Data
 		public string? Summary { get; set; }
 
 		// Поля для эмбеддинга
-		public ProviderType Provider { get; set; }
+		public required ProviderType Provider { get; set; }
 		public required string EmbedderModel { get; set; }
 
-		public Character(string name, string description, string? example = null, string? plans = null, string? summary = null) 
+		public Character(string name, string description, ProviderType providerType, string embedderModel, string? example = null, string? plans = null, string? summary = null) 
 		{
 			Name = name;
 			Description = description;
 			Example = example;
 			Plans = plans;
 			Summary = summary;
+
+			Provider = providerType;
+			EmbedderModel = embedderModel;
 		}
 	}
 }
